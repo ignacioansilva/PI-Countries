655861db5d96a55470f14b8890d9c3867fb29cd0
ignaciansilva <ignacioansilva@gmail.com>
2022-10-03 00:05:25 -0300

    test back funcionando

--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const { expect } = require('chai');
 const session = require('supertest-session');
 const app = require('../../src/app.js');
@@ -6,7 +5,14 @@ const { Country, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const country = {
-  name: 'Argentina',
+  id: "INS",
+  name: "Ignacio´s City",
+  continent: "Henry",
+  subregion: "SubHenry",
+  capital: "Entre Rios",
+  area: "13032003",
+  population: "1",
+  flag: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Bandera_de_la_Provincia_de_Entre_R%C3%ADos.svg/2560px-Bandera_de_la_Provincia_de_Entre_R%C3%ADos.svg.png"
 };
 
 describe('Country routes', () => {
@@ -14,11 +20,20 @@ describe('Country routes', () => {
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Country.sync({ force: true })
-    .then(() => Country.create(pokemon)));
-  describe('GET /countries', () => {
+  beforeEach(() => Country.sync({ force: true }) // reinicio la db
+    .then(() => Country.create(country))); // Envio el country creado como constante
+
+    describe('GET /countries', () => {
     it('should get 200', () =>
       agent.get('/countries').expect(200)
     );
+  });
+  describe('GET /countries/:id', () => {
+    it('should get 200', () =>
+      agent.get('/countries/INS').expect(200)
+    );
+    it('should get 400', () =>
+    agent.get('/countries/NAC').expect(400)
+  );
   });
 });
